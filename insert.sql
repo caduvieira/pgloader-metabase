b99d72432f605069b3cb2e2cbeaca95c2b24c58c
Carlos Vieira <edu.carlos.vieira@gmail.com>
2020-07-06 09:22:35 -0300

    Remove comando COPY

--- insert.sql
+++ insert.sql
@@ -195,10 +195,3 @@ t1.nota,
 t1.dat_avaliacao,
 t1.nome_servico
 );
-
-COPY indicadores(cod_processo, configuracao_orgao_id, protocolo, nome_orgao,
- nome_servico, cod_ciclo, cod_etapa, nome_etapa, cod_tipo_etapa, nome_tipo_etapa,
- ide_finalizado, status_processo, dat_abertura, dat_encerramento, dat_conclusao_etapa,
- nota, dimensao_avaliacao, dat_avaliacao, id_fase_processo, nome_fase_processo,
- cod_servico, cod_orgao, situacao_atual) 
-TO '/indicadores.csv' DELIMITER ',' CSV HEADER;
